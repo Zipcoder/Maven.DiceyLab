d80ab7fe154126eee2366352dd899dc913e81e0e
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-02-26 09:27:35 -0500

    no idea

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,15 @@
 public class Simulation {
 
+    private int dieOne;
+    private int dieTwo;
+
+
+    public simulation(int numberOfDies, long numberOfTosses) {
+        int counter = 0;
+        int diceRoll = Math.random()*6 + numberOfDies;
+
+    }
+
 
 
 }
