f8265ccce34ef2018bad4f9098efd334cb3875e7
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-26 11:49:19 -0500

    getting our template up here

---
+++ src/test/java/BinsTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class BinsTest {
+    @Test
+    public void test() {
+
+    }
+}
---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class DiceTest {
+    @Test
+    public void rollTest() {
+        //Given
+        //When
+        //Then
+    }
+}
