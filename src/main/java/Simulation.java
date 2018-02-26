8b3b6a4d651d90ec371c57ec1754218585bea34c
April Rivera <aprilrivera@zipcoders-MBP-2.fios-router.home>
2018-02-25 21:46:43 -0500

    first thing

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,13 @@
 public class Simulation {
 
+    public static void main(String[] args){
+
+        Dice dice = new Dice();
+
+        for(int i = 0; i < 10; i ++){
+            dice.rollDiceAndPersistValues();
+        }
+    }
 
 
 }
