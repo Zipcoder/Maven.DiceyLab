858e1bc38aad1f76a78a376449e1feaa5875faea
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro-4.local>
2018-02-26 08:52:07 -0500

    ...

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,16 @@
 
 public class Bins {
 
+
+    private int startingBound;
+    private int endingBound;
+
+    private int[] rollSums;
+
+
+
+
+    public Bins(int startingBound, int endingBound) {
+        sumOfRolls = new int[endingBound+1];
+    }
 }
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,18 @@
+
+import java.util.Random;
+
 public class Dice {
 
+    public Dice(Integer numberOfDice){
+        this.numberOfDice = numberOfDice;
+    }
 
+    public Integer tossAndSumTotal (){
+        Random random = new Random();
+    }
+        Integer sumTotalOfToss = 0;
+        for(Integer i = 0; i < numberOfDice i++){
+        sumTotalOfToss += random.nextInt(6)+1;
+        }
+        return sumTotalOfToss;
 }
