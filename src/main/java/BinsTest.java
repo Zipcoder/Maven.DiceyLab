3669a36bb831e420f37e995bbaf05896e97f6619
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro-4.local>
2018-02-26 17:21:02 -0500

    one error left

--- src/main/java/BinsTest.java
+++ src/main/java/BinsTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,9 +8,30 @@ public class BinsTest {
     @Test
     public void testConstructorStartRollandEndingRoll(){
         //Given
-        Bins results = new Bins(2, 12);
+        Bins bins = new Bins(2, 12);
         //When
+        Integer expected = 0;
+        Integer actual = bins.getBin(2);
 
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testGetBin(){
+        //Given
+        Bins bins = new Bins(2, 9);
+        //When
+        Integer expected = 0;
+        Integer actual = bins.getBin(2);
+        //Actual
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddToBin(){
+        //Given
+        Bins bins = new Bins(2, 10);
     }
 
 }
