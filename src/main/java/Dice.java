a98a3138ba282f13b4570630efa243c7f6c06337
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-02-26 17:18:14 -0500

    need to print out results still

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,17 @@
 public class Dice {
 
+    public int numberOfDice;
 
+    public Dice(int numberOfDice) {
+        this.numberOfDice = numberOfDice;
+    }
+
+    public int diceRoll () {
+        int sumOfTotalRolls = 0;
+        for (int i = 0; i < numberOfDice; i++) {
+            sumOfTotalRolls += (int) (Math.random() * 6 + 1);
+        }
+
+        return sumOfTotalRolls;
+    }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,15 +1,40 @@
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Simulation {
 
-    private int dieOne;
-    private int dieTwo;
+    private Integer numberOfThrows;
+    private Dice dice;
+    private Bins bins;
+    private int numberOfDie;
 
-
-    public simulation(int numberOfDies, long numberOfTosses) {
-        int counter = 0;
-        int diceRoll = Math.random()*6 + numberOfDies;
-
+    public Simulation(int numberOfDie, int numberOfThrows) {
+        this.numberOfThrows = numberOfThrows;
+        this.dice = new Dice(2);
+        this.bins = new Bins(numberOfDie, numberOfDie * 6);
     }
 
+//
+//    public void simulationToString() {
+//        StringBuilder sb = new StringBuilder();
+//        for (Map.Entry entry : bins.incrementBin) {
+//            System.out.println(entry + "\n");
+//        }
+//    }
+        public void runSimulation() {
+            for (int i = 0; i < numberOfThrows; i++) {
+                int value = dice.diceRoll();
+                bins.incrementBin(value);
+
+            }
+        }
+
+    public static void main(String[] args) {
+        Simulation sim = new Simulation(2, 10);
+        sim.runSimulation();
+//        sim.simulationToString();
+    }
 
 
 }
