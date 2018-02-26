ae7941028acd8522aa31d6b2a65797dcdd9e451d
Daniel Horowitz <danielhorowitz@zipcoders-MacBook-Pro-80.local>
2018-02-25 21:09:38 -0500

    working through it

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,23 @@
+import java.util.TreeMap;
 
 public class Bins {
 
+    int minIndex;
+    int maxIndex;
+
+    TreeMap<Integer, Integer> results = new TreeMap<Integer, Integer>();
+
+    public Bins (int minIndex, int maxIndex) {
+        results = new TreeMap<Integer, Integer>();
+    }
+
+    public void addCount(int dieNumber, int count) {
+        results.get(dieNumber).(count+1);
+    }
+
 }
+
+
+//    Bins results = new Bins(2, 12); // for bins from 2..12
+//    Integer numberOfTens = results.getBin(10); // returns the number of tens in the 10 bin
+//    results.incrementBin(10); // should increment bin # 10
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,22 @@
-public class Simulation {
+public class Simulation extends Dice {
+
+    int numberOfRolls;
+
+    public Simulation() {
+
+    }
+
+    public Simulation (int numberOfRolls) {
+
+        }
+
+    public void runSimulation (int numberOfRolls) {
 
 
+        for (int i = 0; i < numberOfRolls; i++) {
+            int total = rollDice(2);
 
+        }
+
+    }
 }
