9f163895dd5140c7d19002f164aa2d0cd4b64757
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-05-31 13:01:08 -0400

    no tests written but beginning structure for bins and dice

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,16 @@
+import org.omg.PortableInterceptor.INACTIVE;
 
 public class Bins {
+    private Integer[] bins;
 
+    public Bins(Integer startIndex, Integer endIndex) {
+        bins = new Integer[(endIndex-startIndex)+1];
+    }
+    //how many times you get an integer
+    //Bins(2, 12) creates bins from 2 to 12 and stores number of times you roll a 2 to 12
+
+    public void incrementBin(Integer index){
+        bins[index]++;
+    }
 }
+
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,36 @@
+import com.sun.tools.corba.se.idl.InterfaceGen;
+import org.omg.CORBA.INTERNAL;
+
+import java.net.Inet4Address;
+
+import java.util.Random;
+
 public class Dice {
 
+    Integer[] rolls;
+    Bins bin;
 
+    public Dice(Integer numberOfDice) {
+        rolls = new Integer[numberOfDice];
+        bin = new Bins(numberOfDice, 6*numberOfDice);
+    }
+
+    public Integer[] getRolls() {
+        return rolls;
+    }
+
+    public Integer tossAndSum(){
+        Integer[] rolls = getRolls();
+        Integer sum = 0;
+        for (Integer i = 0; i < rolls.length; i++) {
+            rolls[i] = toss();
+            sum += rolls[i];
+        }
+        return sum;
+    }
+
+    private Integer toss() {
+        Random rnd = new Random();
+        return rnd.nextInt(7)+1;
+    }
 }
