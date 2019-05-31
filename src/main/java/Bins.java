0f2f7cc72a844649010d4de32c1c4a5dd3d0374b
Joanna Hsiao <chsiao58@gmail.com>
2019-05-31 17:49:41 -0400

    Lab is done

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bins {
 
+   private Integer[] bins;
+   private Integer min;
+   private Integer max;
+
+    public Bins(Integer min, Integer max) {
+        this.min = min;
+        this.max = max;
+        bins = new Integer[max+1];
+        for(Integer index = min; index <= max; index++)
+            bins[index] = 0;
+    }
+
+    public Integer getBin(Integer index) {
+        return bins[index];
+    }
+    public void incrementBin(Integer index) {
+        bins[index]++;
+    }
+
+    public Integer getMin() {
+        return min;
+    }
+
+    public Integer getMax() {
+        return max;
+    }
 }
