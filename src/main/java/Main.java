dfc65fb24a8bb390cefc77ad5a543bca5cbce0e8
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-01 09:43:49 -0400

    changed Main to match lab specifications

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Simulation sim = new Simulation(10,1000000000);
+        Simulation sim = new Simulation(2,1000000);
 
         sim.runSim();
 
