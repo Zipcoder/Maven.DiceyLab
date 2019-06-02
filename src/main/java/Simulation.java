a2238684af712dc77904746b6a94b89b29834419
calebPowell-oak <calebpowell.oak@gmail.com>
2019-06-02 03:04:33 -0400

    Simulation runSimulation() finished

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -3,20 +3,22 @@ import com.sun.deploy.uitoolkit.impl.awt.AWTWindowFactory;
 public class Simulation {
     private Integer numberOfDice;
     private Integer numberOfTosses;
+    private Bins bin;
 
     public Simulation(Integer numberOfDice, Integer numberOfTosses){
         this.numberOfDice = numberOfDice;
         this.numberOfTosses = numberOfTosses;
+        bin = new Bins(new Dice(numberOfDice));
     }
     public static void main(String[] args) {
 
     }
 
     public void runSimulation(){
-
+        bin.runTrial(numberOfTosses);
     }
 
     public void getDisplay(){//TODO prints graphical display of normalized dist.
-        
+
     }
 }
