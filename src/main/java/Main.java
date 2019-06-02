1df95d83e2d794f9d6fbe2895b15f9dc5927fca9
calebPowell-oak <calebpowell.oak@gmail.com>
2019-06-02 03:44:26 -0400

    Finished Simulation getDisplay

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Simulation sim = new Simulation(3,10);
+        Simulation sim = new Simulation(3,10000);
         sim.runSimulation();
         sim.getDisplay();
     }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,4 @@
 import com.sun.deploy.uitoolkit.impl.awt.AWTWindowFactory;
-import com.sun.org.apache.xpath.internal.operations.String;
 
 public class Simulation {
     private Integer numberOfDice;
@@ -19,10 +18,20 @@ public class Simulation {
     public void getDisplay(){//TODO prints graphical display of normalized dist.
         StringBuilder output = new StringBuilder();
         Dice d = bin.getDice();
-        for(int i = d.getQuantity(); i < d.getHighestRoll(); i++){
-            output.append(bin.getRawResults()[i]);
+        for(int i = d.getQuantity(); i <= d.getHighestRoll(); i++){
+            Double percent = bin.getRawResults()[i] / new Double(numberOfTosses);
+            output.append(String.format("%4d :%9d :%5.2f ", i, bin.getRawResults()[i], percent));
+            output.append(getStars(percent));
             output.append("\n");
         }
         System.out.println(output.toString());
+    }
+
+    public String getStars(Double multiplier){
+        String stars = "";
+        for(int i = 0; i < (int)(150 * multiplier); i++){
+            stars += "*";
+        }
+        return stars;
     }
 }
