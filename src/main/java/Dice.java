fcc22b0c6bb2fa48eb08a0392ad1c110586de0bc
Brian Patterson <brian@zipcoders-MacBook-Pro-5.local>
2019-10-24 17:23:57 -0400

    Started Generator

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,24 @@
-public class Dice {
+import java.util.Arrays;
+import java.util.Random;
 
+public class Dice {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] diceCounter = new int[13];
+
+        for(int i = 1; i <=1000000; i++){
+            int diceA = random.nextInt(6)+1;
+            int diceB = random.nextInt(6)+1;
+
+            diceCounter[diceA + diceB]++;
+        }
+
+        System.out.println(Arrays.toString(diceCounter));
+    }
 
 }
+
+
+
+
+
