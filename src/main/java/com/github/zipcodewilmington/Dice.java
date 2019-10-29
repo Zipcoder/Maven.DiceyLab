37249e18d97b341398d0afabc0d513840ed7c5b8
Kai Shields <kai@zipcoders-MacBook-Pro-3.local>
2019-10-29 19:29:16 -0400

    checkpoint. added dice die and main application classes

--- src/main/java/com/github/zipcodewilmington/Dice.java
+++ src/main/java/com/github/zipcodewilmington/Dice.java
@@ -1,5 +1,17 @@
 package com.github.zipcodewilmington;
 
 public class Dice {
+    //private final Integer numberOfDie; <- unneded because we can call dice.length will get number of die
+    private final Integer numberOfFaces;
+    private final Die[] dice;
+
+    public Dice(Integer numberOfDie, Integer numberOfFaces) {
+        this.numberOfFaces = numberOfFaces;
+        this.dice = new Die[numberOfDie];
+    }
+
+    public Integer getNumberOfDie() {
+        return dice.length;
+    }
 
 }
