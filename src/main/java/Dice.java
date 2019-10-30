8629b88c50febf6c93662f348e3d86ccf73fcd4c
ushanil <ush.gopi@gmail.com>
2019-10-30 11:02:36 -0400

    done with lab

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,29 @@
-public class Dice {
+import java.util.Random;
+public class Dice
+{
+  /*  Random rand = new Random();
+    int rand_int1 = rand.nextInt(6);
+    int rand_int2 = rand.nextInt(6); */
 
+    private  static final  int  MAX=6;
+
+    private Integer numberOfRolls;
+    private Integer tossSum;
+
+    public Dice(Integer numberOfRolls) {
+        this.numberOfRolls = numberOfRolls;
+    }
+
+
+
+    public Integer tossAndSum()
+    {
+        Integer tossSum =0;
+        for (int i=0; i< numberOfRolls; i++) {
+
+            tossSum += (int) (Math.random() * MAX) + 1;
+        }
+        return tossSum;
+    }
 
 }
