1c61e3dd8a526cf07ae0b2163cd6963868585837
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-3.local>
2019-11-01 22:18:52 -0400

    Work in progress, still has errors

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,31 @@
+import java.util.Map;
+import java.util.HashMap;
+
+
 
 public class Bins {
 
+    private Integer start;
+    private Integer end;
+
+    private Integer[] results;
+
+    public Bins(Integer start, Integer end) {
+        this.start = start;
+        this.end = end;
+
+        Integer endIdx = end + 1;
+        results = new Integer[endIdx];
+
+        for (int i = start - 2; i < results.length; i++) {
+            results[i] = 0;
+        }
+    }
+    public Integer getBin(Integer countOfNum) {
+        return results[countOfNum];
+    }
+
+    public void incrementBin(Integer binIdx) {
+        results[binIdx]++;
+    }
 }
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,27 @@
 public class Dice {
 
 
+    public Dice() {
+    }
+
+
+    public Integer rollDie(Integer numOfDice) {
+
+        Integer result = 0;
+
+        for (int i = 0; i < numOfDice; i++) {
+
+            result += (int) (1 + (Math.random() * 6));
+
+        }
+        return result;
+    }
+    public int roll() {
+        Integer sum = 0;
+        for(int i = 0; i < numOfDice; i++) {
+            Integer die = (int) (Math.random() * 6) + 1;
+            sum += die;
+        }
+        return sum;
+    }
 }
