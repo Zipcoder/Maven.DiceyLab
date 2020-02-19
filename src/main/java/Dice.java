847aa27247d4bab2c24bc4d1a312f9c86136e342
Chris Farmer <cfarmer@zipcodes-MacBook-Pro.local>
2020-02-19 15:10:52 -0500

    finished dice

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,20 @@
 public class Dice {
+    private Integer rolled;
 
 
+    public Dice(Integer rolled) {
+        this.rolled = rolled;
+    }
+
+    public Integer tossAndSum(){
+        Integer sum = 0;
+        for (int i = 1; i <= rolled; i++) {
+            sum += (int) (Math.random()*6) + 1;
+        }
+        return sum;
+    }
+
+    public Integer getRolled() {
+        return rolled;
+    }
 }
