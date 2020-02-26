0beedaf0a254acb541a917e8db0efe5395a53b8e
chipfody <60615532+chipfody@users.noreply.github.com>
2020-02-25 20:21:39 -0500

    Diced

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -21,21 +21,22 @@ public class Simulation {
         }
     }
 
-    public void printResults() {
+   public void printResults() {
         System.out.println("***");
         System.out.println("Simulation of " + numbOfDies + " dice tossed for " + numbOfTosses + " times.");
         System.out.println("***\n");
-        System.out.println("  2:    " + ": " + simBin.getBin(2) + " "  + (simBin.getBin(2) / numbOfTosses) + " ");
-        System.out.println("  3:    " + ": " + simBin.getBin(3));
-        System.out.println("  4:    " + ": " + simBin.getBin(4));
-        System.out.println("  5:    " + ": " + simBin.getBin(5));
-        System.out.println("  6:    " + ": " + simBin.getBin(6));
-        System.out.println("  7:    " + ": " + simBin.getBin(7));
-        System.out.println("  8:    " + ": " + simBin.getBin(8));
-        System.out.println("  9:    " + ": " + simBin.getBin(9));
-        System.out.println(" 10:   " + ": " + simBin.getBin(10));
-        System.out.println(" 11:   " + ": " + simBin.getBin(11));
-        System.out.println(" 12:   " + ": " + simBin.getBin(12));
+        //System.out.println("%3s %10d", " 2 :"   simBin.getBin(2)/numbOfTosses);
+        System.out.printf("%3s%10d%2s%3.2f%-16s",  " 2 :", simBin.getBin(2),": ", (double)simBin.getBin(2)/numbOfTosses," **");
+        System.out.printf("\n%3s%10d%2s%3.2f%-16s",  " 3 :", simBin.getBin(3),": ", (double)simBin.getBin(3)/numbOfTosses," *****");
+        System.out.printf("\n%3s%10d%2s%3.2f%-16s",  " 4 :", simBin.getBin(4),": ", (double)simBin.getBin(4)/numbOfTosses," ********");
+        System.out.printf("\n%3s%10d%2s%3.2f%-16s",  " 5 :", simBin.getBin(5),": ", (double)simBin.getBin(5)/numbOfTosses," ***********");
+        System.out.printf("\n%3s%10d%2s%3.2f%-16s",  " 6 :", simBin.getBin(6),": ", (double)simBin.getBin(6)/numbOfTosses," **************");
+        System.out.printf("\n%3s%10d%2s%3.2f%-16s",  " 7 :", simBin.getBin(7),": ", (double)simBin.getBin(7)/numbOfTosses," *****************");
+        System.out.printf("\n%3s%10d%2s%3.2f%-16s",  " 8 :", simBin.getBin(8),": ", (double)simBin.getBin(8)/numbOfTosses," **************");
+        System.out.printf("\n%3s%10d%2s%3.2f%-16s",  " 9 :", simBin.getBin(9),": ", (double)simBin.getBin(9)/numbOfTosses," ***********");
+        System.out.printf("\n%3s%10d%2s%3.2f%-16s",  "10 :", simBin.getBin(10),": ", (double)simBin.getBin(10)/numbOfTosses," ********");
+        System.out.printf("\n%3s%10d%2s%3.2f%-16s",  "11 :", simBin.getBin(11),": ", (double)simBin.getBin(11)/numbOfTosses," *****");
+        System.out.printf("\n%3s%10d%2s%3.2f%-16s",  "12 :", simBin.getBin(12),": ", (double)simBin.getBin(12)/numbOfTosses," **");
 
 
 
@@ -44,6 +45,7 @@ public class Simulation {
 //    public void printResults() {
 //        StringBuilder output = new StringBuilder();
 //        for (int i = 2; i <=12; i++) {
+//
 //
 //        }
 //    }
