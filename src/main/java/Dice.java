fc37a9a05632ac3a04651051199cac3577150186
xiong <xiong@zipcodes-MBP-8.fios-router.home>
2021-03-06 23:26:31 -0500

    completed lab will refactor code later

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,27 @@
 public class Dice {
+    public int faceValue;
+
+    public Dice() {
+        this.faceValue = (int)((Math.random() * 6) + 1);
+    }
+
+    public static Integer diceToss2() {
+        Dice dice1 = new Dice();
+        Dice dice2 = new Dice();
+
+        return dice1.faceValue + dice2.faceValue;
+    }
+
+//    public static Integer diceToss5() {
+//        Dice dice1 = new Dice();
+//        Dice dice2 = new Dice();
+//        Dice dice3 = new Dice();
+//        Dice dice4 = new Dice();
+//        Dice dice5 = new Dice();
+//
+//        return dice1.faceValue + dice2.faceValue + dice3.faceValue + dice4.faceValue + dice5.faceValue;
+//    }
+
 
 
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,52 @@
 public class Simulation {
 
+    public static void main(String[] args) {
+        Simulation simulate = new Simulation();
+
+        simulate.simulate2Dice(1000000);
+
+
+    }
+
+    public void simulate2Dice(int timesToSimulate) {
+        int counter = 0;
+        Bins results = new Bins();
+        while (counter < timesToSimulate) {
+            results.storeBin(Dice.diceToss2());
+            counter++;
+        }
+        System.out.println(" 2 :     " + results.two + " : percentage **" );
+        System.out.println(" 3 :     " + results.three + " : percentage **" );
+        System.out.println(" 4 :     " + results.four + " : percentage **" );
+        System.out.println(" 5 :    " + results.five + " : percentage **" );
+        System.out.println(" 6 :    " + results.six + " : percentage **" );
+        System.out.println(" 7 :    " + results.seven + " : percentage **" );
+        System.out.println(" 8 :    " + results.eight + " : percentage **" );
+        System.out.println(" 9 :    " + results.nine + " : percentage **" );
+        System.out.println(" 10:     " + results.ten + " : percentage **" );
+        System.out.println(" 11:     " + results.eleven + " : percentage **" );
+        System.out.println(" 12:     " + results.twelve + " : percentage **" );
+    }
+
+//    public void printResults2Dice(int timesToSimulate) {
+//        Bins results = new Bins();
+//        simulate2Dice(timesToSimulate);
+//        System.out.println(" 2 :      " + results.two + ": percentage **" );
+//        System.out.println(" 3 :      " + results.three + ": percentage **" );
+//        System.out.println(" 4 :      " + results.four + ": percentage **" );
+//        System.out.println(" 5 :      " + results.five + ": percentage **" );
+//        System.out.println(" 6 :      " + results.six + ": percentage **" );
+//        System.out.println(" 7 :      " + results.seven + ": percentage **" );
+//        System.out.println(" 8 :      " + results.eight + ": percentage **" );
+//        System.out.println(" 9 :      " + results.nine + ": percentage **" );
+//        System.out.println(" 10:      " + results.ten + ": percentage **" );
+//        System.out.println(" 11:      " + results.eleven + ": percentage **" );
+//        System.out.println(" 12:      " + results.twelve + ": percentage **" );
+//
+//    }
+
+
+
 
 
 }
