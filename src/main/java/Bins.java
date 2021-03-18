f10f7b1e8210ef57888255e3a3f9a1d7e0bb9023
Jbanksalpha <78812718+Jbanksalpha@users.noreply.github.com>
2021-03-17 21:25:50 -0400

    went through dicey lab with tutor

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,22 @@
+import java.util.Map;
+import java.util.HashMap;
+
 
 public class Bins {
+    Map<Integer, Integer> mapOfBins = new HashMap<>();
+
+
+    public Bins(int lowestBinNumber, int highestBinNumber) {
+
+        for (int i = lowestBinNumber; i <= highestBinNumber; i++) {
+            mapOfBins.put(i, 0);
+        }
+    }
+    public Integer getBin(int binNumber) {
+        return mapOfBins.get(binNumber);
+
+
+
+    }
 
 }
